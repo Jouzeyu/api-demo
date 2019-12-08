1d628323477d3ab52c4162715b8fb4632f61ccd6
jouzeyu <jouzeyu@163.com>
2019-12-08 20:19:04 +0800

    接口筛选

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\Api\UserResource;
 use App\User;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 
 class UsersController extends Controller
 {
    public function test(Request $request){
-       $user=User::where('id',2)->first();
-       return $this->success(new UserResource($user));
+       $users = QueryBuilder::for(User::class)
+           ->allowedFilters(['name'])
+           ->get();
+       return UserResource::collection($users);
    }
 }
