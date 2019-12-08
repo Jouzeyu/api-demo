9261cf021c7767f9876d6ece62bd12d14cadcb40
jouzeyu <jouzeyu@163.com>
2019-12-08 14:54:33 +0800

    API登录

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'v1'
+], function () {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('user_info', 'Api\AuthController@userInfo');
+    });
 });
